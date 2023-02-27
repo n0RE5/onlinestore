2ebf73ef573f89a8b8a504f097a6fffe3673197f
n0RE <nore@desktop>
2023-02-27 14:30:12 +0300

    upd: added remove function

--- client/src/http/basketAPI.ts
+++ client/src/http/basketAPI.ts
@@ -9,3 +9,8 @@ export const getBasket = async (basketId: number) => {
     const response = $authHost.get(`api/basket/${basketId}`)
     return response
 }
+
+export const removeBasketItem = async (basketId: number, deviceId: number) => {
+    const response = $authHost.post('api/basket/remove', {basketId, deviceId})
+    return response
+}
