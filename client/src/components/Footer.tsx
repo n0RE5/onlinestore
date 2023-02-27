885422568e40fa398b6c5c791010d56e00759b1e
n0RE <nore@desktop>
2023-02-27 19:34:09 +0300

    upd: footer init

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,13 +1,41 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import classes from './styles/Footer.module.scss'
 
-interface FooterProps {
-    
-}
-
-const Footer: React.FC<FooterProps> = () => {
+const Footer: React.FC = () => {
     return (
-        <div>
-            
+        <div className={classes.footer_w}>
+            <div className={classes.footer_top}>
+                <div className={classes.footer_module}>
+                    <div className={classes.footer_module_name}>INFORMATION</div>
+                    <ul className={classes.footer_module_list}>
+                        <li><Link to="/profile">My account</Link></li>
+                        <li><Link to="/profile?page=basket">Basket</Link></li>
+                        <li><Link to="/profile?page=settings">Settings</Link></li>
+                    </ul>
+                </div>
+                <div className={classes.footer_module}>
+                    <div className={classes.footer_module_name}>OUR OFFERS</div>
+                    <ul className={classes.footer_module_list}>
+                        <li><Link to="/?category=laptop">Laptops</Link></li>
+                        <li><Link to="/?category=smartphone">Smartphones</Link></li>
+                        <li><Link to="/?category=laptop">Tablets</Link></li>
+                        <li><Link to="/?category=camera">Cameras</Link></li>
+                        <li><Link to="/?category=headphones">Headphones</Link></li>
+                    </ul>
+                </div>
+                <div className={classes.footer_module}>
+                    <div className={classes.footer_module_name}>OUR POLICY</div>
+                    <ul className={classes.footer_module_list}>
+                        <li><a href={`mailto:${process.env.REACT_APP_CONTACT_EMAIL}`}>Help & Contact</a></li>
+                        <li><Link to="">Return Policy</Link></li>
+                        <li><Link to="">Terms of use</Link></li>
+                    </ul>
+                </div>
+            </div>
+            <div className={classes.footer_btm}>
+                <div>Copyright &copy; 2023 SHOP. All rights reserved.</div>
+            </div>
         </div>
     );
 };
