78504319eb3dec26233dd01e331404cc941e2c7c
n0RE <nore@desktop>
2023-03-08 13:00:55 +0300

    upd: added profile settings logic

--- client/src/pages/ProfilePages/ProfileSettings.tsx
+++ client/src/pages/ProfilePages/ProfileSettings.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { IRootReducer, IUser } from '../../types/Interfaces';
 
 const ProfileSettings: React.FC = () => {
+    const user = useSelector<IRootReducer, IUser>(state => state.userState.user)
+
     return (
-        <div>
-            
+        <div className="main_w">
+            <div className="main_title">Settings</div>
+            <hr className="main_hr"/>
+            <div className='profile_settings'>
+                <div className='profile_left'>Login</div>
+                <div className='profile_right'>{user.email}</div>
+            </div>
+            <div className='profile_settings'>
+                <div className='profile_left'>Password</div>
+                <div className='profile_right'>*********</div>
+            </div>
         </div>
     );
 };
